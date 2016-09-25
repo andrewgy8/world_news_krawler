3e03eff77591fad7299243a105fa5c561b9a5c34
Andrew Graham-Yooll <andrewgy8@gmail.com>
2016-09-25 18:11:21 +0200

    Got links to all stories on world site

--- krawler.js
+++ krawler.js
@@ -5,34 +5,43 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
+// Load the LODASH .
+var _ = require('cheerio/node_modules/lodash');
+
 
 var pageToVisit = 'https://www.washingtonpost.com/world/';
 
+
 console.log("We are going to be visiting " + pageToVisit + "and extracting the world news from it")
 
-request(pageToVisit, function(error, response, body){
-    if(error){
-        console.log("error:" + error)
-    }
-    console.log("status code: " + response.statusCode);
-    if(response.statusCode === 200){
-        var $ = cheerio.load(body);
-        // var divs = $('div').attr()
-        var linksToNews = $('.story-headline').find('a')
-        console.log("Page section: " + $('section').text());
-        // console.log(linksToNews)
-        // divs.forEach(function(element) {
-        //     console.log(element)
-        // }) 
+    request(pageToVisit, function(error, response, body){
+        if(error){
+            console.log("error:" + error)
+        }
+        console.log("status code: " + response.statusCode);
+        if(response.statusCode === 200){
+            var $ = cheerio.load(body);
+            // var divs = $('div').attr()
+            // var linksToNews = $('.story-headline').find('a')
+            // var newsGroup = $('section').children().last()
+            // console.log("Page section: " + newsGroup);
+            // console.log('link?', newsGroup.length)
+            // var jsonNews = JSON.stringify(newsGroup)
+            // console.log(typeof(jsonNews))
+            // var values2 = newsGroup.map(function(item){
+            //     item.position == 13
+            // })
+            // var values = _.mapValues(newsGroup, function(o){ return o})
+            // console.log(values.prevObject)
+            // console.log(values2)
+            // console.log(jsonNews)
 
-        // for (var key in linksToNews) {
-        //     if (linksToNews.hasOwnProperty(key)) {
-        //         console.log(key + " -> " + linksToNews[key]);
-        //     }
-        // };
-    }
-    collectTheLinks(linksToNews)
-});
+        }
+        collectTheLinks($)
+    });
+
+
+
 
 function searchForLinksToNews($, word){
 
@@ -41,15 +50,18 @@ function searchForLinksToNews($, word){
 function collectTheLinks($){
     var links = [];
 
-    var absoluteLinks = $("a[href^='http']");
+    var absoluteLinks = $("a[href^='https://www.washingtonpost.com/world']");
     absoluteLinks.each(function(){
         links.push($(this).attr('href'));
     })
 
     console.log('found these story links ' + links)
     console.log('there are  links ' + links.length)
+    console.log(links[1])
+
+    console.log('I give eva many kisses?')
+    console.log('of course')
 }
 
-function visitPage(){
 
-}
+
